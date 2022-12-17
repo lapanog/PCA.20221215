416e9d63d6dcf59493e1cf98ffa37c67ffe6daf9
lapanog <120739086+lapanog@users.noreply.github.com>
2022-12-17 14:12:07 -0400

    Create 20221215_001.c

---
+++ 20221215_001.c
@@ -0,0 +1,21 @@
+//20221215_001 Faça um Programa que peça dois números e imprima a soma.
+
+#include <stdio.h>
+
+int main()
+{
+  int num1 = 0;
+  int num2 = 0;
+  int soma = 0;
+  
+  printf("Digite um número: ");
+  scanf("%d", &num1);
+  
+  printf("Digite outro número: ");
+  scanf("%d", &num2);
+  
+  soma = num1 + num2
+  printf("\nO resultado da soma entre %d e %d é %d\n", num1, num2, soma);
+  
+  return 0;
+  }
